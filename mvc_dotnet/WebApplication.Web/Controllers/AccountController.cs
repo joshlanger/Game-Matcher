b53e61e28f2e1005a5badc27018b4085bd0885b7
Melissa Betush <melissabetush@gmail.com>
2019-08-08 10:56:49 -0400

    some functional changes

--- mvc_dotnet/WebApplication.Web/Controllers/AccountController.cs
+++ mvc_dotnet/WebApplication.Web/Controllers/AccountController.cs
@@ -90,11 +90,11 @@ namespace WebApplication.Web.Controllers
 
             return View(registerViewModel);
         }
+
         [HttpGet]
         public IActionResult Profile()
         {
             return View();
         }
-
     }
 }
