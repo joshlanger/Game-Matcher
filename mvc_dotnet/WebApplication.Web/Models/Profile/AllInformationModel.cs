e091fcecb09ca57b670d916d42656c27596aa35d
Josh Langer <joshlanger@gmail.com>
2019-08-13 15:20:36 -0400

    logic for match strength added

--- mvc_dotnet/WebApplication.Web/Models/Profile/AllInformationModel.cs
+++ mvc_dotnet/WebApplication.Web/Models/Profile/AllInformationModel.cs
@@ -12,15 +12,48 @@ namespace WebApplication.Web.Models.Profile
 
         public List<MatchStrengthModel> Matches (List<MatchStrengthModel> allUsers, MatchStrengthModel currentUser)
         {
-            List<MatchStrengthModel> Matches = new List<MatchStrengthModel>();
+            //List<MatchStrengthModel> Matches = new List<MatchStrengthModel>();
+            
+            
+            int totalTitles = currentUser.Title.Count;
+            int totalStyles = 3;
 
-
-            foreach(var user in allUsers)
+            foreach (var user in allUsers)
             {
-                if()
+                int titleCount = 0;
+                int experience = 0;
+                int style = 0;
+
+                foreach (var title in currentUser.Title)
+                {
+                    for (int i = 0; i < user.Title.Count; i++)
+                    {
+                        if (user.Title[i] == title)
+                        {
+                            titleCount++;
+                        }
+                    }
+                }
+                foreach (var gameStyle in currentUser.Style)
+                {
+                    for (int i = 0; i < user.Style.Count; i++)
+                    {
+                        if (user.Style[i] == gameStyle)
+                        {
+                            style++;
+                        }
+                    }
+                }
+                if(user.Experience == currentUser.Experience)
+                {
+                    experience++;
+                }
+                double matchStrength = (titleCount + experience + style)/(totalTitles + totalStyles + 1);
+                user.MatchStrength = matchStrength;
+               
             }
 
-            return Matches;
+            return allUsers;
         }
     }
 }
