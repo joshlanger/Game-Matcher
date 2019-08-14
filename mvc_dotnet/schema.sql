638421938d2eda98bda09635778f4fb768708c18
Greg Kulik <gregkulik@gmail.com>
2019-08-14 16:53:55 -0400

    NEW SCHEMAgit add -A!

--- mvc_dotnet/schema.sql
+++ mvc_dotnet/schema.sql
@@ -38,12 +38,11 @@ CREATE TABLE profile
 (
 profile_id			int				identity(1,1),
 user_id				int				not null,
-user_name			varchar(50)		null,	
+user_name			varchar(50)		not null,	
 avatar_name			varchar(50)		null,		
 user_bio			varchar(250)	null,
-gaming_experience	int				null,
-favorite_genres		varchar(250)	null,
-contact_preference	varchar(50)		not null,
+gaming_experience	varchar(50)		null,
+contact_preference	varchar(50)	    null,
 other_interests		varchar(140)	null,
 is_Private			bit				not null,
 
@@ -84,4 +83,40 @@ image		varchar(50) null,
 
 foreign key (games_id)	references profile_game(games_id)
 );
+COMMIT TRANSACTION;
+
+CREATE TABLE game_library(
+games_id	int			not null,
+title		varchar(50)	null,
+style       varchar(50) null,
+min_players int null,
+max_players int null,
+genre varchar(50) null,
+platform varchar(50) null,
+image		varchar(50) null,
+
+foreign key (games_id)	references profile_game(games_id)
+);
+
+CREATE TABLE genre_library(
+genre_id	int			not null,
+genre		varchar(50)	null,
+);
+
+CREATE TABLE profile_genre(
+profile_genre_id int identity(1,1),
+genre_id	int			not null,
+profile_id	int not null,
+);
+
+CREATE TABLE friends_list(
+friends_list_id int identity(1,1),
+user_id	int			not null,
+profile_id	int not null,
+);
+
+
+
+
+
 COMMIT TRANSACTION;
